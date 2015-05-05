8b0a676e66df07b194bf63d8ebcd2246d9898277
Yrisius <T.Bleeker96@gmail.com>
2015-05-05 14:54:23 +0200

    Update HeightMap.cs
    Added comments

--- GraphicsPractical1/GraphicsPractical1/GraphicsPractical1/HeightMap.cs
+++ GraphicsPractical1/GraphicsPractical1/GraphicsPractical1/HeightMap.cs
@@ -9,10 +9,11 @@ namespace GraphicsPractical1
 {
     class HeightMap
     {
+        // The variables
         private int width, height;
-
         private byte[,] heightData;
 
+        // The construct method of the HeightMap class.
         public HeightMap(Texture2D heightMap)
         {
             this.width = heightMap.Width;
@@ -20,30 +21,35 @@ namespace GraphicsPractical1
 
             this.loadHeightData(heightMap);
         }
-
+        
+        // The loadHeightData method. This method is used to get the color from the texture and fill the heightData.
         private void loadHeightData(Texture2D heightMap)
         {
             this.heightData = new byte[this.width, this.height];
-
+            
             Color[] colorData = new Color[this.width * this.height];
             heightMap.GetData(colorData);
-
+            
+            // For loop in a for loop to check every pixel in the texture.
             for (int x = 0; x < this.width; x++)
                 for (int y = 0; y < this.height; y++)
                     this.heightData[x, y] = colorData[x + y * this.width].R;
         }
-
+        
+        // Property to return or set the heightData byte array element
         public byte this[int x, int y]
         {
             get { return this.heightData[x, y]; }
             set { this.heightData[x, y] = value; }
         }
-
+        
+        // Property to return the width
         public int Width
         {
             get { return this.width; }
         }
-
+        
+        // Property to return the height
         public int Height
         {
             get { return this.height; }
