1a68c30ba5f78faa52cc421468232d3867857bc9
Yrisius <T.Bleeker96@gmail.com>
2015-05-06 09:59:42 +0200

    Done Shit
    It is done

--- GraphicsPractical1/GraphicsPractical1/GraphicsPractical1/Program.cs
+++ GraphicsPractical1/GraphicsPractical1/GraphicsPractical1/Program.cs
@@ -10,7 +10,7 @@ namespace GraphicsPractical1
         /// </summary>
         static void Main(string[] args)
         {
-            using (Game1 game = new Game1())
+            using (Game1 game = new Game1()) 
             {
                 game.Run(); 
             }
